6fe28243fa5b76172b993da2c429796f7580f3db
Alan Ayoub <alanayoub@me.com>
2018-08-17 07:29:18 -0700

    added Psuedo Code for functions

--- js/script.js
+++ js/script.js
@@ -10,19 +10,31 @@ FSJS project 2 - List Filter and Pagination
 // Create a function to hide all of the items in the list excpet for the ten you want to show
 // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
 
+function showPage(/* arguments here for page number and student list */) {
+    // first hide all students on the page
+    // Then loop through all students in our student list argument
+       // if student should be on this page number
+       	// show the student
+ }
+
 
 
 
 // Create and append the pagination links - Creating a function that can do this is a good approach
 
+function appendPageLinks(/* take a student list as an argument */) {
+    // determine how many pages for this student list
+    // create a page link section
+    // “for” every page
+        // add a page link to the page link section
+    // remove the old page link section from the site
+    // append our new page link section to the site
+    // define what happens when you click a link
+        // Use the showPage function to display the page for the link clicked
+        // mark that link as “active”
+}
 
 
 
 // Add functionality to the pagination buttons so that they show and hide the correct items
 // Tip: If you created a function above to show/hide list items, it could be helpful here
-
-
-
-
-
-
